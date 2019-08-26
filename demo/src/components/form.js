5d5a78edfc65fbf3e29b613624f2da12f809a3bf
McSteve7 <stephenajiboye7@gmail.com>
2019-08-26 19:22:40 +0100

    initial commit

---
+++ demo/src/components/form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () => {
+    return ( 
+        <div className="container">
+        <form>
+        <input type = "name">Name</input> 
+        <br />   
+        <input type = "email">Email</input> 
+        <br />
+        <input type = "submit" > Submit </input>  
+        </form> 
+        </div>
+    )
+}
